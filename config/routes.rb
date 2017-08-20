1995394d637215666b0875ee5f3b459abd03c052
Keri Spencer <kerigspencer@gmail.com>
2017-08-20 13:47:55 -0400

    Final push. Moving off of old computer.

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,16 @@ Rails.application.routes.draw do
     post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
 
-  resources :users, only: [:new, :create]
-
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
   root 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show, :create, :update]
+      resources :topics, except: [:edit, :new]
+    end
+  end
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,10 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  let(:name) {RandomData.random_sentence}
-  let(:description) {RandomData.random_paragraph}
-  let(:public) { true }
-  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:topic) { create(:topic) }
 
   it { is_expected.to have_many(:posts) }
 
@@ -13,11 +10,29 @@ RSpec.describe Topic, type: :model do
 
   describe "attributes" do
     it "has name, description, and public attributes" do
-      expect(topic).to have_attributes(name: name, description: description, public: public)
+      expect(topic).to have_attributes(name: topic.name, description: topic.description)
     end
 
     it "is public by default" do
       expect(topic.public).to be(true)
+    end
+  end
+
+  describe "scopes" do
+    before do
+      @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, public: false)
+    end
+
+    describe "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        user = User.new
+        expect(Topic.visible_to(user)).to eq(Topic.all)
+      end
+
+      it "returns only public topics if user is nil" do
+        expect(Topic.visible_to(nil)).to eq([@public_topic])
+      end
     end
   end
 end
